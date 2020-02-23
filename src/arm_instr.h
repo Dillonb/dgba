91fdcbfdd9716a40d2a8c36f8473d8a4855c1ee4
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-02-23 10:38:44 -0500

    Nicer way of determining instruction type

--- src/arm_instr.h
+++ src/arm_instr.h
@@ -23,6 +23,42 @@ typedef enum arm_instr_type {
     SOFTWARE_INTERRUPT
 } arm_instr_type_t;
 
+uint32_t instr_type_masks[] = {
+        0b00001100000000000000000000000000, // DATA_PROCESSING
+        0b00001111110000000000000010010000, // MULTIPLY
+        0b00001111100000000000000010010000, // MULTIPLY_LONG
+        0b00001111101100000000111111110000, // SINGLE_DATA_SWAP
+        0b00001111111111111111111111110000, // BRANCH_EXCHANGE
+        0b00001110010000000000111110010000, // HALFWORD_DT_RO
+        0b00001110010000000000000010010000, // HALFWORD_DT_IO
+        0b00001100000000000000000000000000, // SINGLE_DATA_TRANSFER
+        0b00001110000000000000000000010000, // UNDEFINED
+        0b00001110000000000000000000000000, // BLOCK_DATA_TRANSFER
+        0b00001110000000000000000000000000, // BRANCH
+        0b00001110000000000000000000000000, // CORPROCESSOR_DATA_TRANSFER
+        0b00001111000000000000000000010000, // COPROCESSOR_DATA_OPERATION
+        0b00001111000000000000000000010000, // COPROCESSOR_REGISTER_TRANSFER
+        0b00001111000000000000000000000000, // SOFTWARE_INTERRUPT
+};
+
+uint32_t instr_type_identifiers[] = {
+        0b00000000000000000000000000000000, // DATA_PROCESSING
+        0b00000000000000000000000010010000, // MULTIPLY
+        0b00000000100000000000000010010000, // MULTIPLY_LONG
+        0b00000001000000000000000010010000, // SINGLE_DATA_SWAP
+        0b00000001001011111111111100010000, // BRANCH_EXCHANGE
+        0b00000000000000000000000010010000, // HALFWORD_DT_RO
+        0b00000000010000000000000010010000, // HALFWORD_DT_IO
+        0b00000100000000000000000000000000, // SINGLE_DATA_TRANSFER
+        0b00000110000000000000000000010000, // UNDEFINED
+        0b00001000000000000000000000000000, // BLOCK_DATA_TRANSFER
+        0b00001010000000000000000000000000, // BRANCH
+        0b00001100000000000000000000000000, // CORPROCESSOR_DATA_TRANSFER
+        0b00001110000000000000000000000000, // COPROCESSOR_DATA_OPERATION
+        0b00001110000000000000000000010000, // COPROCESSOR_REGISTER_TRANSFER
+        0b00001111000000000000000000000000, // SOFTWARE_INTERRUPT
+};
+
 
 typedef union arminstr {
     unsigned raw:32; // Used for loading data into this struct
@@ -38,18 +74,18 @@ typedef union arminstr {
                 unsigned rn:4;
                 bool s:1;
                 unsigned opcode:4;
-                unsigned identifier:3; // Equals 0b001 if instruction is this type.
+                unsigned IGNORED:3;
                 arm_cond_t cond:4;
             } DATA_PROCESSING;
             struct {
                 unsigned rm:4;
-                unsigned identifier2:4; // Equals 0b1001 if instruction is this type
+                unsigned IGNORED:4;
                 unsigned rs:4;
                 unsigned rn:4;
                 unsigned rd:4;
                 bool s:1;
                 bool a:1;
-                unsigned identifier:6; // Equals 0b000000 if instruction is this type
+                unsigned IGNORED2:6;
                 arm_cond_t cond:4;
             } MULTIPLY;
             // TODO MULTIPLY_LONG
@@ -57,13 +93,24 @@ typedef union arminstr {
             // TODO BRANCH_EXCHANGE
             // TODO HALFWORD_DT_RO
             // TODO HALFWORD_DT_IO
-            // TODO SINGLE_DATA_TRANSFER
+            struct {
+                unsigned offset:12;
+                unsigned rd:4;
+                unsigned rn:4;
+                bool l:1;
+                bool w:1;
+                bool b:1;
+                bool u:1;
+                bool p:1;
+                unsigned IGNORED:3;
+                arm_cond_t cond:4;
+            } SINGLE_DATA_TRANSFER;
             // TODO UNDEFINED
             // TODO BLOCK_DATA_TRANSFER
             struct {
                 unsigned offset:24; // This value is actually signed, but needs to be this way because of how C works
                 bool l:1;
-                unsigned identifier:3; // Equals 0b101 if instruction is this type
+                unsigned IGNORED:3;
                 arm_cond_t cond:4;
             } BRANCH;
             // TODO COPROCESSOR_DATA_TRANSFER
@@ -74,28 +121,14 @@ typedef union arminstr {
     } parsed;
 } arminstr_t;
 
-bool is_data_processing(arminstr_t* instr) {
-    return instr->parsed.DATA_PROCESSING.identifier == 0b001u;
-}
-
-bool is_multiply(arminstr_t* instr) {
-    return instr->parsed.MULTIPLY.identifier == 0b001u && instr->parsed.MULTIPLY.identifier2 == 0b1001u;
-}
-
-bool is_branch(arminstr_t* instr) {
-    return instr->parsed.BRANCH.identifier == 0b101u;
-}
-
 arm_instr_type_t get_instr_type(arminstr_t* instr) {
-    if (is_data_processing(instr)) {
-        return DATA_PROCESSING;
-    } else if (is_multiply(instr)) {
-        return MULTIPLY;
-    } else if (is_branch(instr)) {
-        return BRANCH;
-    } else {
-        logfatal("Could not determine instruction type! 0x%04X", instr->raw)
+    for (arm_instr_type_t type = DATA_PROCESSING; type <= SOFTWARE_INTERRUPT; type++) {
+        uint32_t masked = instr->raw & instr_type_masks[type];
+        if (masked == instr_type_identifiers[type]) {
+            return type;
+        }
     }
+    logfatal("Could not determine instruction type! 0x%04X", instr->raw)
 }
 
 #endif
