0d889bae578e078945c76183f5fbd874992ef212
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-08 13:40:10 -0400

    Don't fail tests while still in bios

--- tests/test_arm.c
+++ tests/test_arm.c
@@ -5,6 +5,11 @@
 #include "../src/arm7tdmi/arm7tdmi.h"
 #include "../src/gbabus.h"
 #include "../src/gbarom.h"
+#include "../src/gbabios.h"
+
+bool in_bios(arm7tdmi_t* state) {
+    return state->pc < GBA_BIOS_SIZE;
+}
 
 int main(int argc, char** argv) {
     gbamem_t* mem = init_mem();
@@ -22,11 +27,18 @@ int main(int argc, char** argv) {
 
     loginfo("ROM loaded: %lu bytes", mem->rom_size)
     loginfo("Beginning CPU loop")
+    bool bios_complete = false;
     while(true) {
         arm7tdmi_step(cpu);
-        word failed_test = cpu->r[12];
-        if (failed_test != 0) {
-            logfatal("test_arm: FAILED TEST: %d", failed_test)
+        if (!bios_complete && !in_bios(cpu)) {
+            bios_complete = true;
+        }
+
+        if (bios_complete) {
+            word failed_test = cpu->r[12];
+            if (failed_test != 0) {
+                logfatal("test_arm: FAILED TEST: %d", failed_test)
+            }
         }
         // TODO check for when all tests pass
     }
