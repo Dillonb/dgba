05cc85be4798d5199b547fc5d701b41a76b9650a
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-21 18:01:03 -0400

    ADD

--- src/arm7tdmi/thumb_instr/add_subtract.c
+++ src/arm7tdmi/thumb_instr/add_subtract.c
@@ -2,5 +2,20 @@
 #include "../../common/log.h"
 
 void add_subtract(arm7tdmi_t* state, add_subtract_t* instr) {
-    logfatal("add_subtract");
+    word newvalue = get_register(state, instr->rs);
+    if (instr->op) { // subtract
+        logfatal("subtract")
+    } else { // add
+        if (instr->i) {
+            set_flags_add(state, newvalue, instr->rn_or_offset);
+            newvalue += instr->rn_or_offset;
+        } else {
+            word rndata = get_register(state, instr->rn_or_offset);
+            set_flags_add(state, newvalue, rndata);
+            newvalue += get_register(state, instr->rn_or_offset);
+        }
+    }
+
+    set_flags_nz(state, newvalue);
+    set_register(state, instr->rd, newvalue);
 }
