58dbfdf720a07bcf7980101855015c556d6605af
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-21 18:16:42 -0400

    ALU AND + Add comments to rest of alu ops

--- src/arm7tdmi/thumb_instr/alu_operations.c
+++ src/arm7tdmi/thumb_instr/alu_operations.c
@@ -3,55 +3,58 @@
 
 void alu_operations(arm7tdmi_t* state, alu_operations_t* instr) {
     switch (instr->opcode) {
-        case 0x0: { // AND
-            logfatal("AND")
+        case 0x0: { // AND: Rd = Rd AND Rs
+            word newvalue = get_register(state, instr->rd) & get_register(state, instr->rs);
+            set_flags_nz(state, newvalue);
+            set_register(state, instr->rd, newvalue);
+            break;
         }
-        case 0x1: { // EOR
+        case 0x1: { // EOR: Rd = Rd EOR Rs
             logfatal("EOR")
         }
-        case 0x2: { // LSL
+        case 0x2: { // LSL: Rd = Rd << Rs
             logfatal("LSL")
         }
-        case 0x3: { // LSR
+        case 0x3: { // LSR: Rd = Rd >> Rs
             logfatal("LSR")
         }
-        case 0x4: { // ASR
+        case 0x4: { // ASR: Rd = Rd ASR Rs
             logfatal("ASR")
         }
-        case 0x5: { // ADC
+        case 0x5: { // ADC: Rd = Rd + Rs + C
             logfatal("ADC")
         }
-        case 0x6: { // SBC
+        case 0x6: { // SBC: Rd = Rd - Rs - (~C)
             logfatal("SBC")
         }
-        case 0x7: { // ROR
+        case 0x7: { // ROR: Rd = Rd ROR Rs
             logfatal("ROR")
         }
-        case 0x8: { // TST
+        case 0x8: { // TST: Set condition codes on Rd AND Rs
             logfatal("TST")
         }
-        case 0x9: { // NEG
+        case 0x9: { // NEG: Rd = -Rs
             logfatal("NEG")
         }
-        case 0xA: { // CMP
+        case 0xA: { // CMP: Set condition codes on Rd - Rs
             logfatal("CMP")
         }
-        case 0xB: { // CMN
+        case 0xB: { // CMN: Set condition codes on Rd + Rs
             logfatal("CMN")
         }
-        case 0xC: { // ORR
+        case 0xC: { // ORR: Rd = Rd | Rs
             word newvalue = get_register(state, instr->rd) | get_register(state, instr->rs);
             set_flags_nz(state, newvalue);
             set_register(state, instr->rd, newvalue);
             break;
         }
-        case 0xD: { // MUL
+        case 0xD: { // MUL: Rd = Rd * Rs
             logfatal("MUL")
         }
-        case 0xE: { // BIC
+        case 0xE: { // BIC: Rd = Rd & ~Rs
             logfatal("BIC")
         }
-        case 0xF: { // MVN
+        case 0xF: { // MVN: Rd = ~Rs
             logfatal("MVN")
         }
         default:
