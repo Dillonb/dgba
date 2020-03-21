227bdeb0d93484a8d3253fcc2aaa3f29ebff9290
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-21 16:03:58 -0400

    LSL

--- src/arm7tdmi/thumb_instr/move_shifted_register.c
+++ src/arm7tdmi/thumb_instr/move_shifted_register.c
@@ -1,15 +1,27 @@
 #include "move_shifted_register.h"
 #include "../../common/log.h"
+#include "../shifts.h"
 
 void move_shifted_register(arm7tdmi_t* state, move_shifted_register_t* instr) {
     switch (instr->opcode) {
         case 0: { // LSL
-            logfatal("LSL")
+            word value = get_register(state, instr->rs);
+            if (instr->offset == 0) {
+                // No shift performed and carry flag not updated
+                value = arm_shift_special_zero_behavior(state, &state->cpsr, LSL, value);
+            } else {
+                value = arm_shift(&state->cpsr, LSL, value, instr->offset);
+            }
+            set_register(state, instr->rd, value);
+            set_flags_nz(state, value);
+            break;
         }
         case 1: { // LSR
+            unimplemented(instr->offset == 0, "LSR by 0 amount")
             logfatal("LSR")
         }
         case 2: { // ASR
+            unimplemented(instr->offset == 0, "ASR by 0 amount")
             logfatal("ASR")
         }
         default:
