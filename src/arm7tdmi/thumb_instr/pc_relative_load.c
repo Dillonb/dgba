dbf549533192396a4f353e70caea5e32e3abbafc
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-22 10:13:45 -0400

    PC relative load

--- src/arm7tdmi/thumb_instr/pc_relative_load.c
+++ src/arm7tdmi/thumb_instr/pc_relative_load.c
@@ -2,5 +2,10 @@
 #include "../../common/log.h"
 
 void pc_relative_load(arm7tdmi_t *state, pc_relative_load_t *instr) {
-    logfatal("pc_relative_load")
+    word offset = instr->word8 << 2;
+    word maskedpc = state->pc & 0xFFFFFFFC;
+
+    word address = maskedpc + offset;
+
+    set_register(state, instr->rd, state->read_word(address));
 }
