d1968792ef1cdb0369120405cbae6141c04d8863
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-22 08:07:36 -0400

    Highreg add

--- src/arm7tdmi/thumb_instr/high_register_operations.c
+++ src/arm7tdmi/thumb_instr/high_register_operations.c
@@ -8,10 +8,24 @@ void high_register_operations(arm7tdmi_t* state, high_register_operations_t* ins
         unimplemented(instr->opcode == 0b10, "Invalid flag combination!")
     }
     switch (instr->opcode) {
-        case 0b00: // ADD
-            logfatal("Unimplemented opcode: ADD")
-        case 0b01: // CMP (only one that sets condition codes in this group)
+        case 0b00: { // ADD
+            int adj_rd = instr->h1 ? instr->rdhd + 8 : instr->rdhd;
+            int adj_rs = instr->h2 ? instr->rshs + 8 : instr->rshs;
+            word rsdata = get_register(state, adj_rs);
+            if (adj_rs == 15) {
+                rsdata = (rsdata + 2) & 0xFFFFFFFE;
+            }
+            word rddata = get_register(state, adj_rd);
+            if (adj_rd == 15) {
+                rddata = (rsdata + 2) & 0xFFFFFFFE;
+            }
+            word result = rsdata + rddata;
+            set_register(state, adj_rd, result);
+            break;
+        }
+        case 0b01: { // CMP (only one that sets condition codes in this group)
             logfatal("Unimplemented opcode: CMP")
+        }
         case 0b10: { // MOV
             int adj_rd = instr->h1 ? instr->rdhd + 8 : instr->rdhd;
             int adj_rs = instr->h2 ? instr->rshs + 8 : instr->rshs;
