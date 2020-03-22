d23238bce0dd30dd7dcd658d3603c996f614dde8
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-22 11:59:03 -0400

    load_store_io

--- src/arm7tdmi/thumb_instr/load_store.c
+++ src/arm7tdmi/thumb_instr/load_store.c
@@ -23,6 +23,26 @@ void load_store_ro(arm7tdmi_t* state, load_store_ro_t* instr) {
         }
     }
 }
+
 void load_store_io(arm7tdmi_t* state, load_store_io_t* instr) {
-    logfatal("load_store_io")
+    word offset = instr->offset << (instr->b ? 0 : 2);
+    word address = get_register(state, instr->rb) + offset;
+    if (instr->l) {
+        if (instr->b) {
+            word value = state->read_byte(address);
+            set_register(state, instr->rd, value);
+        } else {
+            word value = state->read_word(address);
+            value = arm_ror(NULL, value, (address & 3u) << 3);
+            set_register(state, instr->rd, value);
+        }
+    } else {
+        word value = get_register(state, instr->rd);
+        if (instr->b) {
+            byte bytevalue = value & 0xFF;
+            state->write_byte(address, bytevalue);
+        } else {
+            state->write_word(address, value);
+        }
+    }
 }
