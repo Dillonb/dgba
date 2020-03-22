7e50bf3bb65c830bf6533cae074044870270ca63
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-22 14:10:52 -0400

    Update DISPSTAT with v/hblank flags

--- src/ppu.c
+++ src/ppu.c
@@ -12,13 +12,30 @@ gba_ppu_t* init_ppu() {
     return ppu;
 }
 
+bool is_hblank(gba_ppu_t* ppu) {
+    return ppu->x > GBA_SCREEN_X;
+}
+
+bool is_vblank(gba_ppu_t* ppu) {
+    return ppu->y > GBA_SCREEN_Y && ppu->y != 227;
+}
+
 void ppu_step(gba_ppu_t* ppu) {
+    // Update coords and set V/HBLANK flags
     ppu->x++;
+    if (!ppu->DISPSTAT.hblank && is_hblank(ppu)) {
+        ppu->DISPSTAT.hblank = true;
+    }
     if (ppu->x >= GBA_SCREEN_X + GBA_SCREEN_HBLANK) {
         ppu->x = 0;
+        ppu->DISPSTAT.hblank = false;
         ppu->y++;
+        if (!ppu->DISPSTAT.vblank && is_vblank(ppu)) {
+            ppu->DISPSTAT.vblank = true;
+        }
         if (ppu->y > GBA_SCREEN_Y + GBA_SCREEN_VBLANK) {
             ppu->y = 0;
+            ppu->DISPSTAT.vblank = false;
         }
     }
 }
