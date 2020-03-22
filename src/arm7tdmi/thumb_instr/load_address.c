674c8094433d8db48ca01226e1d75d7276263dab
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-03-22 07:29:40 -0400

    shift and mask

--- src/arm7tdmi/thumb_instr/load_address.c
+++ src/arm7tdmi/thumb_instr/load_address.c
@@ -4,9 +4,11 @@
 void load_address(arm7tdmi_t* state, load_address_t* instr) {
     unimplemented(instr->sp == 1, "Loading an address to the SP")
 
+    half offset = instr->word8 << 2;
+
     if (instr->sp) {
-        set_register(state, instr->rd, get_sp(state) + instr->word8);
+        set_register(state, instr->rd, get_sp(state) + offset);
     } else {
-        set_register(state, instr->rd, state->pc + instr->word8);
+        set_register(state, instr->rd, (state->pc & 0xFFFFFFFD) + offset);
     }
 }
