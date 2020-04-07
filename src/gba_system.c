3b061f1875a4b418c994f5f288021f70d94333de
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-04-06 23:57:20 -0400

    Make timers a little faster

--- src/gba_system.c
+++ src/gba_system.c
@@ -1,7 +1,7 @@
 #include "mem/gbabus.h"
 #include "mem/gbarom.h"
 #include "mem/gbabios.h"
-#include "timer.h"
+#include "common/log.h"
 
 int cycles = 0;
 
@@ -30,6 +30,51 @@ void init_gbasystem(const char* romfile, const char* bios_file) {
     bus = init_gbabus();
 }
 
+
+int timer_freq[4] = {1, 64, 256, 1024};
+
+void timer_tick(int cyc) {
+    bool previous_overflow = false;
+    for (int n = 0; n < 4; n++) {
+        bool overflow = false;
+
+        if (bus->TMCNT_H[n].start) {
+            unimplemented(bus->TMCNT_H[n].cascade, "Timer cascade")
+            if (previous_overflow && bus->TMCNT_H[n].cascade) {
+                logfatal("Timer cascade - need to inc this timer here, and ONLY here. (don't inc normally)")
+            }
+
+            if (!bus->TMINT[n].previously_enabled) {
+                bus->TMINT[n].previously_enabled = true;
+                bus->TMINT[n].ticks = 0;
+                bus->TMINT[n].value = bus->TMCNT_L[n].timer_reload;
+            }
+
+            if (bus->TMCNT_H[n].cascade && !previous_overflow) {
+                previous_overflow = false;
+                continue; // Don't inc
+            }
+
+            bus->TMINT[n].ticks += cyc;
+
+            while (bus->TMINT[n].ticks >= timer_freq[bus->TMCNT_H[n].frequency]) {
+                bus->TMINT[n].ticks -= timer_freq[bus->TMCNT_H[n].frequency];
+                if (bus->TMINT[n].value == 0xFFFF) {
+                    bus->TMINT[n].value = bus->TMCNT_L[n].timer_reload;
+                    overflow = true;
+                    unimplemented(bus->TMCNT_H[n].timer_irq_enable, "IRQ on timer overflow")
+                } else {
+                    bus->TMINT[n].value++;
+                }
+            }
+        } else {
+            bus->TMINT[n].previously_enabled = false;
+        }
+
+        previous_overflow = overflow;
+    }
+}
+
 void gba_system_step() {
     int dma_cycles = gba_dma();
     cpu->irq = (bus->interrupt_enable.raw & bus->IF.raw) != 0;
@@ -43,10 +88,10 @@ void gba_system_step() {
         }
     }
 
-    timer_tick(cycles);
 
     while (cycles > 4) {
         ppu_step(ppu);
+        timer_tick(4);
         cycles -= 4;
     }
 }
--- src/timer.h
+++
@@ -1,4 +0,0 @@
-#ifndef GBA_TIMER_H
-#define GBA_TIMER_H
-void timer_tick(int cycles);
-#endif //GBA_TIMER_H
