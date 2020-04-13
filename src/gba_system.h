eae3f027c5c9144214a5d61736efa5a63c24b1dd
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-04-13 17:09:19 -0400

    Add back logtester

--- src/gba_system.h
+++ src/gba_system.h
@@ -5,6 +5,8 @@
 #include "graphics/ppu.h"
 #include "mem/gbabus.h"
 
+extern bool cpu_stepped;
+
 extern arm7tdmi_t* cpu;
 extern gba_ppu_t* ppu;
 extern gbabus_t* bus;
@@ -12,5 +14,6 @@ extern gbamem_t* mem;
 extern bool should_quit;
 
 void init_gbasystem(const char* romfile, const char* bios_file);
+void gba_system_step();
 void gba_system_loop();
 #endif //GBA_GBA_SYSTEM_H
